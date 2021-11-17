35b3bcc1e0d5fbaab5fb55b734d03647d966114c
FlaviaDosReis <reis.dos.flavia@gmail.com>
2021-11-16 21:55:44 -0300

    Base de Dados Artigos

--- Case_Dasa/case_artigos.js
+++
@@ -1,5 +0,0 @@
-//Instalar:  npm install --save sequelize
-// npm install --save pg
-// npm install --save pg-hstore
-/* acessar o site: https://www.elephantsql.com/ para acessar o banco de dados
-Model, view 
---
+++ routes/artigos.routes.js
@@ -0,0 +1,16 @@
+// rotas do sistema de artigos
+
+/* GET: 
+    _obter todos os artigos
+    _obter uma rtigo específico
+    _obter todos os artigos publicados
+
+POST: 
+    _criar um novo artigo
+
+PUT:
+    _publicar meu artigo
+
+DELETE: 
+    _deletar meu artigo
+*/
